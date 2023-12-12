19def3b0c66de5b9fd62ae485a2fc15e98ba733f
hmshoberg <145800386+hmshoberg@users.noreply.github.com>
2023-12-12 16:43:27 -0600

    Troubleshooting

--- surfsup/app.py
+++ surfsup/app.py
@@ -2,8 +2,10 @@
 import os
 import numpy as np
 import pandas as pd
-import datetime as dt
+import datetime as datetime
 import sqlalchemy
+import sqlite3
+import os
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect, desc
@@ -87,32 +89,40 @@ def stations():
 #Create tobs Route
 @app.route("/api/v1.0/tobs")
 def tobs():
-     session = Session(engine)
+    session = Session(engine)
      
-     session.query(Measurement.station, func.count(Measurement.station).label('station_count'))\
-                    .group_by(Measurement.station).order_by(desc('station_count')).all()
-     most_active_station = session.query(measurement.station, func.count(measurement.station)).\
-                    order_by(func.count(measurement.station).desc()).\
-                    group_by(measurement.station).first()[0]
+#Query for most active station
+    most_active_station = session.query(Measurement.station, func.count(Measurement.station).label\
+                    ('station_count')).group_by(Measurement.station).order_by(desc('station_count')).\
+                    first()[0]
 
-     session.query(func.min(Measurement.tobs).label('lowest_temperature'),
-                    func.max(Measurement.tobs).label('highest_temperature'),
-                    func.avg(Measurement.tobs).label('average_temperature'))\
-                   .filter(Measurement.station == most_active).first()
+#Query for temperature statistics for the most active station
+    temperature_stats = session.query(func.min(Measurement.tobs).label('lowest_temperature'),
+                                      func.max(Measurement.tobs).label('highest_temperature'),
+                                      func.avg(Measurement.tobs).label('average_temperature'))\
+                    .filter(Measurement.station == most_active_station).first()
 
 #Query for Previous 12 months
 
-     recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-     last_date = session.query(func.max(Measurement.date)).scalar()
-     most_recent_date = dt.datetime.strptime(last_date, '%Y-%m-%d') - dt.timedelta(days=365)
-     query_date = most_recent_date.strftime('%Y-%m-%d')
+    recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    last_date = session.query(func.max(Measurement.date)).scalar()
+    most_recent_date = datetime.datetime.strptime(last_date, '%Y-%m-%d') - datetime.timedelta(days=365)
 
-     results = session.query(Measurement.tobs).filter(Measurement.date.between\
+    results = session.query(Measurement.tobs).filter(Measurement.date.between\
                     (most_recent_date,last_date),Measurement.station == 'USC00519281').all()
 
-     session.close()
+    session.close()
 
-     return jsonify(most_active_station)
+#Prepare the result as a dictionary
+    result = {
+        'temperature_statistics': {
+            'lowest_temperature': temperature_stats.lowest_temperature,
+            'highest_temperature': temperature_stats.highest_temperature,
+            'average_temperature': temperature_stats.average_temperature
+        },
+        'temperature_results': results
+    }
+    return jsonify(result)
 
 #Create start date and end date
 
